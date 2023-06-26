0e8feb293a4dcaea6fdaa9d83b7b0f3683e946d8
Sakshi Upadhyay <76247870+Sak-creator@users.noreply.github.com>
2023-06-26 16:41:48 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -17,19 +17,19 @@ class GenerateSQLQueryAction(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        print("tracker.latest_message")
-        t=next(tracker.get_latest_entity_values("table"), None)
-        print(t)
+       # print("tracker.latest_message")
+        #t=next(tracker.get_latest_entity_values("table"), None)
+       # print(t)
         dispatcher.utter_message(Text='done')
         entity_value = tracker.get_slot("table")
-        print('entity')
+        #print('entity')
         dispatcher.utter_message(entity_value)
         intent = tracker.latest_message.get("intent").get("name")
         dispatcher.utter_message(intent)
         table = tracker.get_slot("table")
-        print(tracker.latest_message)
+        #print(tracker.latest_message)
         if intent == "provide_table":
-             sql_query = "SELECT * FROM {table} ORDER BY sales DESC LIMIT 10"
+             sql_query = f"SELECT * FROM {table} ORDER BY sales DESC LIMIT 10"
              dispatcher.utter_message(template="utter_display_results", query=sql_query)
         else:
              sql_query = "default"
